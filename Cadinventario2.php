bf7aecc2bc714a9c0ef28ad014f1ef52b75b1a6c
Thiago2436 <thiagomartins.florestal@gmail.com>
2022-05-02 16:57:58 -0300

    Conformidade e responsividade do CSS da pagina Cadinventarios

--- Cadinventario2.php
+++ Cadinventario2.php
@@ -1,4 +1,18 @@
 
+<!DOCTYPE html>
+<html lang="Pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Sstilo.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1> CADASTRO DE INVENTARIO</h1>
+    </header>
+   <nav> 
 
 <?php
     session_start();
@@ -10,7 +24,12 @@
     $Rua = $_POST['Rua'];
     $Numero = $_POST['Numero'];
     $Complemento = $_POST['Complemento'];
-  
+echo "Cidade : ". $Cidade."<br>";
+echo "Estado : ". $Estado."<br>";  
+echo "Bairro : ". $Bairro."<br>";
 
 
 ?>
+</nav>
+</body>
+</html>
